71e0ac37553d7aa1c52b13c5321f821bea288d24
dakodabutnot <southforwinter6@gmail.com>
2020-03-12 22:41:17 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "dev.dakoda"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.junit.jupiter:junit-jupiter:5.5.2")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
